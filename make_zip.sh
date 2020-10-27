46950c2adbc0bfc5cbe16e2e7e68e0138c9e008f
julian-heng <julianhengwl@gmail.com>
2020-10-28 02:37:11 +0800

    Update make_zip.sh

--- make_zip.sh
+++ make_zip.sh
@@ -5,10 +5,12 @@ main()
 {
     shopt -s globstar
     shopt -s nullglob
+
     files=("task.sh" "src"/**/*.py)
 
+    declaration="DeclarationOfOriginality_v1.1.pdf"
     report_fname="heng_julian_19473701_mp_assignment_2_report.pdf"
-    report="report/${report_fname}"
+    report="report/report.pdf"
     if [[ ! -e "${report}" ]]; then
         (
             cd report
@@ -21,9 +23,9 @@ main()
         exit 1
     fi
 
-    cp "${report}" "${report_fname}"
+    gs -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -sOUTPUTFILE="${report_fname}" "${declaration}" "${report}"
 
-    files+=("${report_fname}")
+    files+=("${declaration}" "${report_fname}")
     zip "heng_julian_19473701.zip" "${files[@]}"
 }
 
